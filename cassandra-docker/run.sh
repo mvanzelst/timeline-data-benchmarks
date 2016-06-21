30c5725c7cfb650ee0382f2903f48218c3f0c2ed
Marijn van Zelst <marijn.vanzelst@trifork.nl>
2016-06-21 12:27:33 +0200

    Running the benchmarks in docker did not work for me

--- cassandra-docker/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo docker run --name timeseries-benchmark-cassandra -i -t -v /media/marijn/data/cassandra-timeseries-benchmark:/var/lib/cassandra timeseries-benchmark/cassandra
--- mongodb-docker/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo docker run --name timeseries-benchmark-mongodb -i -t timeseries-benchmark/mongodb
